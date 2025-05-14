0ab9edb14d9f9bb8a5b32d325d6c20f0f3302012
medialabdivulga <medialabdivulga@ucm.es>
2025-05-14 03:35:44 +0100

    Create script.js

---
+++ visitavirtual/script.js
@@ -0,0 +1,42 @@
+pannellum.viewer('panorama', 
+ {   
+    "default": {
+        "firstScene": "circle",
+        "sceneFadeDuration": 1000
+    },
+
+    "scenes": {
+        "circle": {
+            "hfov": 110,
+            "pitch": -3,
+            "yaw": 117,
+            "type": "equirectangular",
+            "panorama": "https://raw.githubusercontent.com/medialabdivulga/conectomavirtual360/refs/heads/main/actividades/assets/img/visitaRaquel1.jpg",
+            "hotSpots": [
+                {
+                    "pitch": -2.1,
+                    "yaw": 45.9,
+                    "type": "scene",
+                    "sceneId": "house"
+                }
+            ]
+        },
+
+        "house": {
+            "hfov": 110,
+            "yaw": 5,
+            "type": "equirectangular",
+            "panorama": "https://raw.githubusercontent.com/medialabdivulga/conectomavirtual360/refs/heads/main/actividades/assets/img/visitaRaquel2.jpg",
+            "hotSpots": [
+                {
+                    "pitch": -0.6,
+                    "yaw": 37.1,
+                    "type": "scene",
+                    "sceneId": "circle",
+                    "targetYaw": -23,
+                    "targetPitch": 2
+                }
+            ]
+        }
+    }
+});
